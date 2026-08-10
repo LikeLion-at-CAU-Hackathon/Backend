4175e7256f236e5dd5fdd3daa7a522ae3e4b7221
yoon3660 <yoon3660@cau.ac.kr>
2026-08-11 03:13:46 +0900

    feat: 3.1 StyleProfile API 구현(mock 데이터)

--- recommendations/urls.py
+++ recommendations/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import VisitHistoryCreateAPIView, VisitSessionCreateAPIView
+from .views import StyleAnalyzeAPIView, VisitHistoryCreateAPIView, VisitSessionCreateAPIView
 
 
 urlpatterns = [
@@ -14,5 +14,11 @@ urlpatterns = [
     "sessions/<int:session_id>/history/",
     VisitHistoryCreateAPIView.as_view(),
     name="visit-history-create",
-),
+    ),
+
+    path(
+    "sessions/<int:session_id>/analyze/",
+    StyleAnalyzeAPIView.as_view(),
+    name="style-analyze",
+    ),
 ]
--- recommendations/views.py
+++ recommendations/views.py
@@ -6,9 +6,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-from .models import VisitSession, VisitHistory
+from .models import StyleProfile, VisitSession, VisitHistory
 from products.models import Product
-from .serializers import VisitHistorySerializer, VisitSessionSerializer
+from .serializers import StyleProfileSerializer, VisitHistorySerializer, VisitSessionSerializer
 
 
 class VisitSessionCreateAPIView(APIView):
@@ -56,4 +56,53 @@ class VisitHistoryCreateAPIView(APIView):
         return Response(
             serializer.data,
             status=status.HTTP_201_CREATED
+        )
+
+class StyleAnalyzeAPIView(APIView):
+
+    def post(self, request, session_id):
+        visit_session = get_object_or_404(
+            VisitSession,
+            id=session_id
+        )
+
+        histories = VisitHistory.objects.filter(
+            visit_session=visit_session
+        )
+
+        if not histories.exists():
+            return Response(
+                {
+                    "success": False,
+                    "message": "분석할 제품이 없습니다."
+                },
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        if histories.count() == 1:
+            mode = StyleProfile.AnalysisMode.SINGLE_PRODUCT
+        else:
+            mode = StyleProfile.AnalysisMode.BEHAVIOR
+
+        profile, created = StyleProfile.objects.update_or_create(
+            visit_session=visit_session,
+            defaults={
+                "summary": "Current browsing activity shows an interest in refined and versatile styling.",
+                "tags": [
+                    "Warm Tone Interest",
+                    "Compact",
+                    "Classic"
+                ],
+                "analysis_mode": mode
+            }
+        )
+
+        serializer = StyleProfileSerializer(profile)
+
+        return Response(
+            {
+                "success": True,
+                "data": serializer.data
+            },
+            status=status.HTTP_200_OK
         )
